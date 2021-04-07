a2477bd7d40b250a76ef3fe0ce9aa547a514e62b
Hardik Parwana <hardiksp@umich.edu>
2021-04-06 22:00:28 -0400

    source the setup file for pythonpath

---
+++ setup_export.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export PYTHONPATH=${PWD}/:${PYTHONPATH}
+export PYTHONPATH=${PWD}/r3t/:${PYTHONPATH}
+export PYTHONPATH=${PWD}/pypolycontain/:${PYTHONPATH}
+export PYTHONPATH=${PWD}/polytope_symbolic_system/:${PYTHONPATH}
+export PYTHONPATH=${PWD}/closest_polytope_algorithms/:${PYTHONPATH}
